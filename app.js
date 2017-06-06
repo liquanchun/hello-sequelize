f0433fc535cf9b1c028cbc3032e994be0729281d
Vongola <1085280247@qq.com>
2017-06-06 21:45:37 +0800

    设置数据库映射关系

--- app.js
+++ app.js
@@ -10,3 +10,17 @@ let sequelize = new Sequelize(config.database, config.username, config.password,
         idle: 30000
     }
 })
+var Pet = sequelize.define('pet', {
+	id: {
+		type: Sequelize.STRING(50),
+        primaryKey: true
+	},
+	name: Sequelize.STRING(100),
+    gender: Sequelize.BOOLEAN,
+    birth: Sequelize.STRING(10),
+    createdAt: Sequelize.BIGINT,
+    updatedAt: Sequelize.BIGINT,
+    version: Sequelize.BIGINT
+}, {
+	timestamps: false
+})
