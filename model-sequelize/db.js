78cbd0bdca518bfbcec59bb71695e123ff5c61c4
Vongola <1085280247@qq.com>
2017-06-08 22:12:35 +0800

    初始话表结构 定义方法便于新建表合并初始设定

--- model-sequelize/db.js
+++ model-sequelize/db.js
@@ -54,4 +54,77 @@ function defineModel(name, attributes) {
         type: Sequelize.BIGINT,
         allowNull: false
     };
+    console.log('model defined for table: ' + name + '\n' + JSON.stringify(attrs, function (k, v) {
+        if (k === 'type') {
+            for (let key in Sequelize) {
+                if (key === 'ABSTRACT' || key === 'NUMBER') {
+                    continue;
+                }
+                let dbType = Sequelize[key];
+                if (typeof dbType === 'function') {
+                    if (v instanceof dbType) {
+                        if (v._length) {
+                            return `${dbType.key}(${v._length})`;
+                        }
+                        return dbType.key;
+                    }
+                    if (v === dbType) {
+                        return dbType.key;
+                    }
+                }
+            }
+        }
+        return v;
+    }, '  '));
+    // 返回格式化数据库格式
+    console.log(sequelize.define);
+    return sequelize.define(name, attrs, {
+        tableName: name,
+        timestamps: false,
+        hooks: {
+            // 数据变化时调用钩子
+            beforeValidate: function (obj) {
+                let now = Date.now();
+                // 如果是新的纪录，添加初始数据
+                if (obj.isNewRecord) {
+                    console.log('will create entity...' + obj);
+                    if (!obj.id) {
+                        // 没有id时添加唯一id
+                        obj.id = genetateId();
+                    }
+                    obj.createdAt = now;
+                    obj.updatedAt = now;
+                    obj.version = 0;
+                } else {
+                    console.log('will update entity...');
+                    obj.updatedAt = now;
+                    obj.version++;
+                }
+            }
+        }
+    });
 }
+
+var exp = {
+    defineModel: defineModel,
+    sync: () => {
+        // 只有在非生产环境下才能创建ddl
+        if (process.env.NODE_ENV !== 'production') {
+           return sequelize.sync({force: true});
+        } else {
+            throw new Error ('Cannot sync() when NODE_ENV is set to \'production\'.');
+        }
+    }
+};
+
+// 定义数据类型
+const TYPES = ['STRING', 'INTEGER', 'BIGINT', 'TEXT', 'DOUBLE', 'DATEONLY', 'BOOLEAN'];
+
+for (let type of TYPES) {
+    exp[type] = Sequelize[type];
+}
+// 将唯一id暴露
+
+exp.ID = ID_TYPE;
+exp.STRING 
+module.exports = exp;
